f4d8045a54351229cd9ae214c22298c04a8be67e
ryulurala@gmail.com <ryulurala@gmail.com>
2019-10-18 04:01:31 +0900

    chracter control and increase number

---
+++ 디자인 자료/personal_project/Assets/Scripts/BarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarController : MonoBehaviour
+{
+    public Text numberText;
+    public Image whiteBar;
+
+    private float whiteBarWidth;
+
+    [SerializeField]
+    private int maxNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteBarWidth = whiteBar.rectTransform.sizeDelta.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        whiteBar.rectTransform.sizeDelta = new Vector2(float.Parse(numberText.text) / maxNumber * whiteBarWidth , whiteBar.rectTransform.sizeDelta.y);
+    }
+}
---
+++ 디자인 자료/personal_project/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Animator mAnimator;
+
+    private NavMeshAgent mNavMeshAgent;
+
+    private bool mRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                mNavMeshAgent.destination = hit.point;
+            }
+        }
+
+        if(mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            mRunning = false;
+        }
+        else
+        {
+            mRunning = true;
+        }
+
+        mAnimator.SetBool("running", mRunning);
+    }
+}
